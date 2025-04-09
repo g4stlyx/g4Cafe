bf3608331e471b5f93e755a0944edd9a46d1c15f
g4stlyx <g4stlyonline@gmail.com>
2025-04-09 16:49:34 +0300

    deployment attepmt 4

--- App.jsx
+++ App.jsx
@@ -1,43 +1,40 @@
-import { useContext, useEffect } from 'react'
+import { useContext, useEffect } from "react";
 import { Route, Routes, HashRouter } from "react-router-dom";
 
-import ProductsContext from './context/productsContext'
+import ProductsContext from "./context/productsContext";
 
-import Dashboard from './components/Dashboard'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import Menu from './components/Menu';
-
+import Dashboard from "./components/Dashboard";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Menu from "./components/Menu";
 
 function App() {
+  const { fetchCategories, categories } = useContext(ProductsContext);
 
-  const {fetchCategories,categories} = useContext(ProductsContext)
-
-  useEffect(()=>{
-    fetchCategories()
-  },[])
+  useEffect(() => {
+    fetchCategories();
+  }, []);
 
   return (
-    <div className='App'>
-          <Header />
-          <Routes>
-          <Route path="/" exact element={<Dashboard />} />
-          <Route path="/menu" element={<Menu />} />
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/" exact element={<Dashboard />} />
+        <Route path="/menu" element={<Menu />} />
 
-          {
-              categories.map(category=>{
-                  <Route
-                  key={category.category_name}
-                  path={"/menu#"+category.category_name}
-                  element={<Menu chosenCategoryByUser={category}/>}
-                  />
-              })
-          }
-          
-          </Routes>
-          <Footer />
+        {categories.map((category) => {
+          return (
+            <Route
+              key={category.category_name}
+              path={"/menu/" + category.category_name}
+              element={<Menu chosenCategoryByUser={category} />}
+            />
+          );
+        })}
+      </Routes>
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
